8b0cd1641f611c7263f4909ed4b3c5e3db1b6eb6
Elliot Crowder <elliot.k.crowder@gmail.com>
2025-11-20 15:37:19 -0800

    reading data file

--- node.js
+++ node.js
@@ -5,43 +5,52 @@
 //if every node conntains the edges to all of its adjacent nodes
 //then we end up with duplicate vavlues being stored whereas
 //if we use an adjacency matrix we only need one source of truth
+function loadNodes() {
+	console.log("bad");
+	const nodes = fetch("/front-end/data.json").then((response) =>
+		response.json()
+	);
+	console.log("success");
+	console.log(nodes);
+}
+console.log("ss");
+loadNodes();
 
 //example change
 class graph {
-  constructor() {
-    this.nodes = [];
-	this.numNodes = 0;
-  }
-
-  getNodes() {
-	return this.nodes;
-  }
-
-  getNode(nodeName) {
-	for (node of this.nodes) {
-		if (node.getName() === nodeName) {
-			return node;
-		}
+	constructor() {
+		this.nodes = [];
+		this.numNodes = 0;
 	}
-	throw new Error("Node not found!");
-  }
 
-  getDistanceBetween(node1Name, node2Name) {
+	getNodes() {
+		return this.nodes;
+	}
+
+	getNode(nodeName) {
+		for (node of this.nodes) {
+			if (node.getName() === nodeName) {
+				return node;
+			}
+		}
+		throw new Error("Node not found!");
+	}
+
+	getDistanceBetween(node1Name, node2Name) {
 		const node1 = this.getNode(node1Name);
 		const node2 = this.getNode(node2Name);
 		return node1.getDistanceTo(node2);
-  }
+	}
 
-  createNode(xCoord, yCoord, nodeName) {
-	const newNode = new node(xCoord, yCoord, nodeName);
-	this.nodes.push(newNode);
-	this.numNodes += 1;
-	//console.log(this.nodes);
-	// Later implement test for duplicates
-  }
+	createNode(xCoord, yCoord, nodeName) {
+		const newNode = new node(xCoord, yCoord, nodeName);
+		this.nodes.push(newNode);
+		this.numNodes += 1;
+		//console.log(this.nodes);
+		// Later implement test for duplicates
+	}
 
 	deleteNode(nodeName) {
-		
 		for (let i = 0; i < this.nodes.length; i++) {
 			if (this.nodes[i].getName() === nodeName) {
 				if (i === this.nodes.length - 1) {
@@ -55,33 +64,27 @@ class graph {
 				}
 			}
 		}
-		
 	}
 
 	getNeighborNodes(node) {
 		let neighborNodes = [];
 		for (let i = 0; i < node.edges.length; i++) {
-			if (node.edges[i].node1.getName() === node.getName() || node.edges[i].node2.getName() === node.getName()) {
+			if (
+				node.edges[i].node1.getName() === node.getName() ||
+				node.edges[i].node2.getName() === node.getName()
+			) {
 				neighborNodes.push(node.edges[i].getOppositeNode(node));
-				
-			}
-			else {
+			} else {
 				console.log("no");
 			}
-
-
 		}
 		return neighborNodes;
-
-
-
 
 		// for all edges of node
 		// if either node1 or node2 = this.node
 		// return getOppositeNode(this.node)
 
 		//}
-
 	}
 
 	addEdge(weight, node1Name, node2Name) {
@@ -93,37 +96,35 @@ class graph {
 	}
 }
 
-
-
 class node {
-  constructor(xCoord, yCoord, nodeName) {
-    this.xCoord = xCoord;
-    this.yCoord = yCoord;
-    this.name = nodeName;
-    this.edges = [];
-  }
+	constructor(xCoord, yCoord, nodeName) {
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.name = nodeName;
+		this.edges = [];
+	}
 
-  addEdge(edge) {
-    this.edges.push(edge);
-  }
-  getName() {
-    return this.name;
-  }
-  getDistanceTo(node) {
-    //should this function assume that all nodes being requested
-    //to are properly connected?
-    for (edge of this.edges) {
-      if (
-        edge.getNodes().includes(this.name) &&
-        edge.getNodes().includes(node.getName())
-      ) {
-        return edge.weight;
-      } else {
-        continue;
-      }
-    }
-    return -1;
-  }
+	addEdge(edge) {
+		this.edges.push(edge);
+	}
+	getName() {
+		return this.name;
+	}
+	getDistanceTo(node) {
+		//should this function assume that all nodes being requested
+		//to are properly connected?
+		for (edge of this.edges) {
+			if (
+				edge.getNodes().includes(this.name) &&
+				edge.getNodes().includes(node.getName())
+			) {
+				return edge.weight;
+			} else {
+				continue;
+			}
+		}
+		return -1;
+	}
 }
 
 class edge {
@@ -138,8 +139,10 @@ class edge {
 		return [this.node1.getName(), this.node2.getName()];
 	}
 	getOppositeNode(node) {
-
-		if (node.getName() != this.node1.getName() && node.getName() != this.node2.getName()) {
+		if (
+			node.getName() != this.node1.getName() &&
+			node.getName() != this.node2.getName()
+		) {
 			throw new Error("Invalid node");
 		}
 		return node.getName() === this.node1.getName() ? this.node2 : this.node1;
@@ -150,7 +153,6 @@ function Dijkstras(graph, sourceNodeName) {
 	let queue = [];
 	let prev = {};
 	let dists = {};
-
 
 	for (const node of graph.getNodes()) {
 		dists[node.getName()] = Infinity;
@@ -168,15 +170,20 @@ function Dijkstras(graph, sourceNodeName) {
 	// queue.pop()
 	// console.log("Sorted queue: ", queue);
 
-	 while (queue.length > 0) {
-	 	queue.sort((a, b) => {a[0] - b[0]});
+	while (queue.length > 0) {
+		queue.sort((a, b) => {
+			a[0] - b[0];
+		});
 		const [currentDist, currentNode] = queue.pop();
 		if (currentDist > dists[currentNode.getName()]) {
 			continue;
 		}
 		const currentNodeNeighbors = graph.getNeighborNodes(currentNode);
 		for (neighbor of currentNodeNeighbors) {
-			const weight = graph.getDistanceBetween(currentNode.getName(), neighbor.getName());
+			const weight = graph.getDistanceBetween(
+				currentNode.getName(),
+				neighbor.getName()
+			);
 			const newDist = dists[currentNode.getName()] + weight;
 			if (newDist < dists[neighbor.getName()]) {
 				dists[neighbor.getName()] = newDist;
@@ -184,10 +191,9 @@ function Dijkstras(graph, sourceNodeName) {
 				queue.push([newDist, neighbor]);
 			}
 		}
-	 }
+	}
 
-	 return [dists, prev];
-
+	return [dists, prev];
 }
 
 let graph1 = new graph();
@@ -205,13 +211,7 @@ graph1.addEdge(1, "3", "4");
 //console.log(graph1.nodes[0].getName());
 console.log(Dijkstras(graph1, "0"));
 
-
-
-
 //console.log(graph1.nodes[2].getName());
-
-
-
 
 // let nas = new node(5, 5,  "Natural Science Building");
 // let RHN = new node(10, 10, "Rodda Hall North");
@@ -236,4 +236,3 @@ console.log(Dijkstras(graph1, "0"));
 // for (neighbor of neighbors) {
 // 	console.log(neighbor.getName());
 // }
-
